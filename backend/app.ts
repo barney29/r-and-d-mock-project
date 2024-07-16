ed690346fb9e045d353c569d386f02d784cb655f
barney29 <barnabasbelay13@gmail.com>
2024-07-16 08:20:21 +0300

    clearing path

--- backend/app.ts
+++ backend/app.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from "express";
 import globalErrorHandler from "./features/global.error.handler";
 import { authRouter } from "./router/auth.router.";
 import { usersRouter } from "./router/users.router";
+import { postRouter } from "./router/post.router";
+
 const app = express();
 
 const v1 = "/api/v1";
 app.use(express.json());
 app.use(`${v1}/auth`, authRouter);
 app.use(`${v1}/users`, usersRouter);
+app.use(`${v1}/posts`, postRouter);
 
 app.all("*", (req: Request, res: Response) => {
   res.status(404).json({
--- backend/router/users.router.ts
+++ backend/router/users.router.ts
@@ -1,6 +1,17 @@
 import { Router } from "express";
-import { get_all_user } from "../controller/user.controller";
+import {
+  delete_user,
+  get_all_user,
+  get_user_by_id,
+  update_user,
+} from "../controller/user.controller";
 
-export const usersRouter = Router()
+export const usersRouter = Router();
 
-usersRouter.route("/").get(get_all_user)
+usersRouter.route("/").get(get_all_user);
+
+usersRouter
+  .route("/:id")
+  .get(get_user_by_id)
+  .put(update_user)
+  .delete(delete_user);
